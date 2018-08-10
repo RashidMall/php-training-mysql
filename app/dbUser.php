2aa805073b9efc0dc6ad4e0ecae5cdedb6b4b878
RashidMall <37588770+RashidMall@users.noreply.github.com>
2018-08-10 09:57:16 +0200

    Update dbUser.php

--- app/dbUser.php
+++ app/dbUser.php
@@ -1,6 +1,6 @@
 <?php 
 // MySQL version
-/* $connection = mysqli_connect('den1.mysql1.gear.host', 'users25', 'Yp4UBm15b~x!', 'users25');
+/* $connection = mysqli_connect('host', 'db', 'pwd', 'user');
 
 if(!$connection){
     die("Database connection failed!");
@@ -8,7 +8,7 @@ if(!$connection){
 
 // PDO version
 try{
-    $connectionUser = new PDO('mysql:host=den1.mysql1.gear.host;dbname=users25', 'users25', 'Yp4UBm15b~x!');
+    $connectionUser = new PDO('mysql:host=myhost.host;dbname=mydbname', 'myname', 'pwd');
 }catch(PDOException $e){
     echo "Database connection failed: " . $e->getMessage;
     die();
